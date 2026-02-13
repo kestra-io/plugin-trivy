764787372778a5181b064a4c3e97faf0f175a3ab
François Delbrayelle <fdelbrayelle@gmail.com>
2026-02-13 08:35:41 +0100

    chore: update plugin categories (#20)

--- src/main/java/io/kestra/plugin/trivy/cli/package-info.java
+++ src/main/java/io/kestra/plugin/trivy/cli/package-info.java
@@ -1,4 +1,5 @@
-@PluginSubGroup(title = "Trivy", categories = PluginSubGroup.PluginCategory.TOOL)
+@PluginSubGroup(title = "Trivy",categories = { PluginSubGroup.PluginCategory.DATA, PluginSubGroup.PluginCategory.INFRASTRUCTURE }
+)
 package io.kestra.plugin.trivy.cli;
 
 import io.kestra.core.models.annotations.PluginSubGroup;
